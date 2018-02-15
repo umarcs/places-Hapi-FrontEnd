584e1e018a7c9d93c43eb46d8389a4bdd84940b7
UmarFarooq <umarfarooq.2201@gmail.com>
2018-02-15 16:01:00 +0500

    add

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <Switch> 
 
 
-          <Route path='/dashboard' component={View} />
+          <Route path='/dashboassssrd' component={View} />
 
           <Route path='/' render={() =>
               <p>
